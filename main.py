a17ff6e446b2468ea7138fd51967a810e1090974
Kashish Srivastava <cannibalcheeseburger@pop-os.localdomain>
2020-04-06 12:35:52 +0530

    added main menu and some tweaks

--- main.py
+++ main.py
@@ -8,17 +8,21 @@ import os
 def main():
     while(True):
         choose = int(input("1.Add Series\n2.Display Cached Series List\n3.Update List\n0.Quit\nEnter your choice(0-3): "))
+        os.system("clear")
         if choose == 1:
             AddNew.add_series()
-            elif choose == 2:
-                display.display_series()
-            elif choose == 3:
-                UpdateCurrent.update_series()
-            elif choose == 0:
-                return 
-            else:
-                print("Invalid choice")
-                os.system('clear')
+            input("Press anything to proceed:")
+        elif choose == 2:
+            display.display_series()
+            input("Press anything to proceed:")
+        elif choose == 3:
+            UpdateCurrent.update_series()
+            input("Press anything to proceed:")
+        elif choose == 0:
+            return 
+        else:
+            input("Invalid choice\nPress anything to proceed:")
+        os.system('clear')
 
 if __name__ == "__main__":
     main()   
