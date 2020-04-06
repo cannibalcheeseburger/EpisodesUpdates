63bcba20f4547027699a41d591992f8b78255688
Kashish Srivastava <cannibalcheeseburger@pop-os.localdomain>
2020-04-06 20:46:23 +0530

    adding delete function

--- display.py
+++ display.py
@@ -1,10 +1,8 @@
-import datetime
 from texttable import Texttable
 import pandas as pd
 
 def display_series():   
     df =pd.read_csv("./csv/Current.csv",index_col=[0])
-    today_date = datetime.datetime.today()
     table = Texttable()
     table.header(["Name","Ep_name","Season","Aired on"])
     for index,row in df.iterrows():
